7fec6e43a9463bb4a95389ca4f00e658ad0a67db
hung0125 <65654501+hung0125@users.noreply.github.com>
2021-02-21 12:22:06 +0800

    Add files via upload

--- Python/fileGuard/fileGuard.py
+++ Python/fileGuard/fileGuard.py
@@ -270,12 +270,14 @@ def ask():
         print('Execute from CMD: fileGuard.exe Encrypt "C:\Windows\System32" "e+:jpg/pdf/exe"')
         print("\n\n\nChoices:\n(1) Encrypt\n(2) Decrypt")
         action = input('Your choice (input number): ')
-        timeStart = int(time())
+        timeStart = 0
         if action == "1":
+            timeStart = int(time())
             print("Encrypting...\n")
             encrypt("", "")
             #record starting ts
         elif action == "2":
+            timeStart = int(time())
             print("Decrypting...\n")
             decrypt("", "")
             
