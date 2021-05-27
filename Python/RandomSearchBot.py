04d6a25ea60bbfeae8cf221ef514b38d1e24e304
hung0125 <65654501+hung0125@users.noreply.github.com>
2021-05-27 22:38:11 +0800

    Create RandomSearchBot.py
    This python script can kill your 90% of spare time, use it with caution!

---
+++ Python/RandomSearchBot.py
@@ -0,0 +1,20 @@
+import requests as rq
+from random import randrange as rr
+from random import choice
+import time
+
+strElement = [0] * 128
+for i in range(128):
+    strElement[i] = i
+
+count = 0
+while True:
+    howLong = rr(1,80)
+    randStr = ""
+    for i in range(howLong):
+        randStr += chr(choice(strElement))
+
+    open("Google.html", 'wb').write(rq.get(f"https://www.google.com/search?q={randStr}").content)
+    count += 1
+    print(f"#{count}: I searched:\n {randStr}\n\n_____________________")
+    time.sleep(3)
